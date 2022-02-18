03958a513c9635e88c18a71ccc2a17b947be1deb
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-02-18 21:00:18 +0700

    style(Layout.tsx): :lipstick: add active link behavior

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,34 +1,41 @@
+import { useRouter } from "next/router";
 import React from "react";
-import { AiFillHome } from "react-icons/ai";
-import { FaPencilRuler, FaRocket } from "react-icons/fa";
-import { RiAccountCircleFill } from "react-icons/ri";
 
 import FooterComponent from "@/components/FooterComponent";
 import HeaderComponent from "@/components/HeaderComponent";
+import UnstyledLink from "@/components/links/UnstyledLink";
+import { menuList } from "@/constants/MenuList";
+import clsxm from "@/lib/helpers/clsxm";
+import { menuIcon } from "@/lib/helpers/menuIcon";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
   return (
     <section>
       <HeaderComponent />
       <div className="layout-container">{children}</div>
       <FooterComponent />
-      <div className="fixed bottom-4 left-1/2 flex h-auto w-9/12 -translate-x-1/2 items-center justify-around rounded-lg bg-slate-300 py-2 shadow-lg dark:bg-slate-600 md:hidden">
-        <div className="flex flex-col items-center justify-center gap-2">
-          <AiFillHome className="text-lg" />
-          <h5 className="font-bold">Home</h5>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <FaRocket className="text-lg" />
-          <h5 className="font-bold">Projects</h5>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <FaPencilRuler className="text-lg" />
-          <h5 className="font-bold">Articles</h5>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <RiAccountCircleFill className="text-lg" />
-          <h5 className="font-bold">About Me</h5>
-        </div>
+      <div
+        className={clsxm(
+          "fixed bottom-4 left-1/2 flex w-11/12 -translate-x-1/2 items-center justify-around",
+          "rounded-lg bg-slate-300 py-2 opacity-90 shadow-xl",
+          "dark:bg-slate-600 md:w-9/12 lg:hidden"
+        )}
+      >
+        {menuList.map((menu, index) => (
+          <UnstyledLink
+            key={index}
+            href={menu.route}
+            className={clsxm(
+              "flex flex-col items-center justify-center gap-2 p-1",
+              "hover:rounded-md hover:border hover:border-primary-100",
+              router.pathname === menu.route && "border border-primary-100"
+            )}
+          >
+            {menuIcon(menu.menu_name)}
+            <h5 className="font-bold">{menu.menu_name}</h5>
+          </UnstyledLink>
+        ))}
       </div>
     </section>
   );
