9b2e78d8a0f3f2cebb83a135d98ef85b631881db
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-02-21 20:09:11 +0700

    feat(articles): :sparkles: initiate base article list page

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,11 +1,105 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+import React, { useState } from "react";
 
+import UnstyledSelect, {
+  SingleOptionList,
+} from "@/components/forms/UnstyledSelect";
 import Layout from "@/components/layouts/Layout";
+import UnstyledLink from "@/components/links/UnstyledLink";
+import { DEFAULT_IMG_ARTICLE } from "@/constants/baseConstants";
+import clsxm from "@/lib/helpers/clsxm";
+import { formatDate } from "@/lib/helpers/formatDate";
+import { getArticleList } from "@/lib/services/fetcher";
+import {
+  Articles as ArticlesType,
+  SingleArticleInList,
+  SingleRes,
+} from "@/lib/services/types";
 
-const Articles = () => {
+export async function getStaticProps() {
+  const articleList = await getArticleList();
+
+  return {
+    props: {
+      articleList,
+    },
+    revalidate: 60,
+  };
+}
+
+const Articles = ({ articleList }: { articleList: ArticlesType }) => {
+  const languageOptions: Array<SingleOptionList> = [
+    {
+      labelName: "English",
+      value: "en",
+    },
+    {
+      labelName: "Indonesian",
+      value: "idn",
+    },
+  ];
+
+  const [language, setLanguage] = useState<string>("en");
+
+  const filterByLanguage = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    return setLanguage(e.target.value);
+  };
+
   return (
     <Layout>
       <h1>Articles</h1>
+      <p className="my-2">
+        Just some random thoughts. For me, writting can sharpen my understanding
+        of something.
+      </p>
+
+      <UnstyledSelect
+        optionList={languageOptions}
+        wrapperClassName="flex items-center gap-3 my-4"
+        labelName="Choose Language"
+        defaultValue={language}
+        className="w-2/12 rounded-lg"
+        onChange={filterByLanguage}
+      />
+
+      <div className="flex flex-col gap-3">
+        {articleList
+          .filter(
+            (article: SingleRes<SingleArticleInList>) =>
+              article.fields.lang === language
+          )
+          .map((article, index) => (
+            <UnstyledLink
+              key={index}
+              className="group"
+              href={`/articles/${article.fields.slug}`}
+            >
+              <div
+                className={clsxm(
+                  "h-42 flex items-center gap-3 p-4 md:h-48",
+                  "rounded-md border-2 border-primary-200 dark:border-primary-500",
+                  "duration-200 ease-in group-hover:-translate-y-1",
+                  "group-hover:ring group-hover:ring-zinc-400"
+                )}
+              >
+                <img
+                  alt="article-images"
+                  src={
+                    article.fields.article_image
+                      ? article.fields.article_image[0].url
+                      : DEFAULT_IMG_ARTICLE
+                  }
+                  className="w-32 rounded-sm md:w-44"
+                  loading="lazy"
+                />
+                <div className="block space-y-4 group-hover:underline">
+                  <h3>{article.fields.title}</h3>
+                  <p>{formatDate(article.fields.date)}</p>
+                </div>
+              </div>
+            </UnstyledLink>
+          ))}
+      </div>
     </Layout>
   );
 };
