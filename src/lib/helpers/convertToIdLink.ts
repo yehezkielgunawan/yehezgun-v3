4afd1a86b53da04a914643cdfe95d7c69d9dee3b
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-02-24 21:33:16 +0700

    refactor(convertToIdLink): :fire: remove convertToId function

--- src/lib/helpers/convertToIdLink.ts
+++
@@ -1,8 +0,0 @@
-export const convertToId = (heading: string) =>
-  heading
-    ?.split(/[\s,]+/)
-    .join("-")
-    .toLowerCase();
-
-export const convertToIdLink = (currentPath: string, heading: string) =>
-  `${currentPath.split("#")[0]}#${convertToId(heading)}`;
