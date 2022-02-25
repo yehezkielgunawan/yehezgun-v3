7858b6a5ac14c710a879ddfa12399713bebcf0f2
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-02-25 21:12:30 +0700

    feat(newTheme): :lipstick: add syntax highliter

--- src/components/markdown/newTheme.tsx
+++ src/components/markdown/newTheme.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useState } from "react";
+import { FaCheckCircle, FaCopy } from "react-icons/fa";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { nightOwl } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 import clsxm from "@/lib/helpers/clsxm";
 
+import Button from "../buttons/Button";
 import UnderlineLink from "../links/UnderlineLink";
 import UnstyledLink from "../links/UnstyledLink";
 
@@ -59,11 +64,6 @@ export const newTheme = {
       {...props}
     />
   ),
-  pre: (props: React.ComponentPropsWithRef<"pre">) => (
-    <pre className="bg-gray-200 p-2 text-xs" {...props}>
-      {props.children}
-    </pre>
-  ),
   li: (props: React.ComponentPropsWithRef<"li">) => {
     return <li className="my-2 ml-4 list-disc">{props.children}</li>;
   },
@@ -71,7 +71,7 @@ export const newTheme = {
     return (
       <blockquote
         className={clsxm(
-          "rounded-md bg-cyan-200 p-4 dark:bg-cyan-500",
+          "rounded-md bg-cyan-200 py-1 px-2 dark:bg-cyan-500",
           "text-black dark:text-white",
           "border-l-8 border-blue-500 dark:border-blue-600",
           "shadow-md dark:shadow-gray-300"
@@ -83,5 +83,63 @@ export const newTheme = {
   },
   p: (props: React.ComponentPropsWithRef<"p">) => {
     return <p className="my-4">{props.children}</p>;
+  },
+  code: (props: React.ComponentPropsWithRef<"code">) => {
+    const childrenValue = String(props.children).replace(/\n$/, "");
+    const language = props.className?.replace(/language-/, "");
+    const [isCopied, setIsCopied] = useState<boolean>(false);
+
+    if (!language) {
+      return (
+        <code className="rounded bg-slate-200 p-0.5 dark:bg-slate-600">
+          {props.children}
+        </code>
+      );
+    }
+    return (
+      <>
+        <SyntaxHighlighter
+          language={language}
+          style={nightOwl}
+          showLineNumbers={false}
+        >
+          {childrenValue}
+        </SyntaxHighlighter>
+        <Button
+          className={clsxm(
+            "absolute top-0 right-0 -translate-y-1/2",
+            "border-none bg-teal-500 hover:bg-teal-400 active:bg-teal-600"
+          )}
+          onClick={() => {
+            navigator.clipboard.writeText(childrenValue);
+            setIsCopied(true);
+            setTimeout(() => {
+              setIsCopied(false);
+            }, 3000);
+          }}
+        >
+          {!isCopied ? (
+            <FaCopy size={20} className="mr-2" />
+          ) : (
+            <FaCheckCircle size={20} className="mr-2" />
+          )}{" "}
+          Copy
+        </Button>
+        {language && (
+          <p
+            className={clsxm(
+              "absolute top-0 -translate-y-1/2",
+              "bg-slate-200 p-0.5 dark:bg-slate-600",
+              "rounded-md"
+            )}
+          >
+            {language}
+          </p>
+        )}
+      </>
+    );
+  },
+  pre: (props: React.ComponentPropsWithRef<"pre">) => {
+    return <pre className="relative my-8 pt-5">{props.children}</pre>;
   },
 };
