5067ef6173104dba45b0a605c0f7da99e62ec6bf
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-02-26 13:40:38 +0700

    feat(MetaHead ang yehezOgImage): :sparkles: initiate base NextSeo for each page

---
+++ src/components/layouts/MetaHead.tsx
@@ -0,0 +1,46 @@
+import { NextSeo } from "next-seo";
+
+import yehezgunOgImage from "@/lib/helpers/yehezOgImage";
+
+type MetaHeadProps = {
+  pageTitle: string;
+  pageDesc: string;
+  route?: string;
+  isArticle: boolean;
+};
+
+const MetaHead = ({ pageTitle, pageDesc, route, isArticle }: MetaHeadProps) => {
+  const ogImage = yehezgunOgImage(pageTitle, isArticle, pageDesc);
+  return (
+    <NextSeo
+      title={`${pageTitle} | YehezGun`}
+      description={pageDesc}
+      canonical={`https://yehezgun.com/${route ? route : ""}`}
+      openGraph={{
+        url: `https://yehezgun.com/${route ? route : ""}`,
+        title: pageTitle,
+        description: pageDesc,
+        type: "website",
+        images: [
+          {
+            url: `${ogImage}`,
+          },
+        ],
+        site_name: "yehezgun.com",
+      }}
+      twitter={{
+        handle: "@handle",
+        site: "@site",
+        cardType: "summary_large_image",
+      }}
+      additionalLinkTags={[
+        {
+          rel: "icon",
+          href: "/peep_yehez.png",
+        },
+      ]}
+    />
+  );
+};
+
+export default MetaHead;
--- src/lib/helpers/yehezOgImage.ts
+++ src/lib/helpers/yehezOgImage.ts
@@ -1,8 +1,8 @@
-export const yehezOgImage = (
+export default function yehezOgImage(
   text: string,
   isArticle: boolean,
   desc: string
-) => {
+): string {
   if (isArticle) {
     return `https://og.yehezgun.com/api/base?description=An%20article%20post%20by%20Yehezkiel%20Gunawan&logo=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&logoHeight=150&logoWidth=150&siteName=yehezgun.com&templateTitle=${encodeURIComponent(
       text
@@ -13,4 +13,4 @@ export const yehezOgImage = (
   )}&siteName=yehezgun.com&templateTitle=${encodeURIComponent(
     text
   )}&theme=dark`;
-};
+}
