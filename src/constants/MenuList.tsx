38575945a60e0d8ee51458794485ef5468a33b39
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-02-26 12:17:23 +0700

    refactor(MenuList.tsx): :recycle: change icon data type to IconType

--- src/constants/MenuList.tsx
+++ src/constants/MenuList.tsx
@@ -1,33 +1,33 @@
-import React from "react";
 import { AiFillHome } from "react-icons/ai";
 import { FaPencilRuler, FaRocket } from "react-icons/fa";
+import { IconType } from "react-icons/lib";
 import { RiAccountCircleFill } from "react-icons/ri";
 
 type MenuListProps = {
   menu_name: string;
   route: string;
-  icon: React.ReactNode;
+  icon: IconType;
 };
 
 export const menuList: Array<MenuListProps> = [
   {
     menu_name: "Home",
     route: "/",
-    icon: <AiFillHome className="text-xl" />,
+    icon: AiFillHome,
   },
   {
     menu_name: "Projects",
     route: "/projects",
-    icon: <FaRocket className="text-xl" />,
+    icon: FaRocket,
   },
   {
     menu_name: "Articles",
     route: "/articles",
-    icon: <FaPencilRuler className="text-xl" />,
+    icon: FaPencilRuler,
   },
   {
     menu_name: "About Me",
     route: "/aboutme",
-    icon: <RiAccountCircleFill className="text-xl" />,
+    icon: RiAccountCircleFill,
   },
 ];
