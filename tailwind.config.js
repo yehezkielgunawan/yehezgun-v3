0fce75bcf018cb0aacc5949960e13c77e6c6980a
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-02-27 19:49:29 +0700

    chore(tailwind.config.js): :wrench: change dark background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,7 @@ module.exports = {
           800: withOpacityValue("--tw-color-primary-800"),
           900: withOpacityValue("--tw-color-primary-900"),
         },
-        dark: "#222222",
+        dark: "#1A202C",
       },
     },
   },
