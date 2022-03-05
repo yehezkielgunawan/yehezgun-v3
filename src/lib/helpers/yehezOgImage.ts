25ca339c21df3e44d556d6e62abd13bbe98beee9
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-03-05 12:17:03 +0700

    style(yehezOgImage): :lipstick: adjust the OG Image to the newest one for article post

--- src/lib/helpers/yehezOgImage.ts
+++ src/lib/helpers/yehezOgImage.ts
@@ -4,7 +4,7 @@ export default function yehezOgImage(
   desc: string
 ): string {
   if (isArticle) {
-    return `https://og.yehezgun.com/api/base?description=An%20article%20post%20by%20Yehezkiel%20Gunawan&logo=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&logoHeight=150&logoWidth=150&siteName=yehezgun.com&templateTitle=${encodeURIComponent(
+    return `https://og.yehezgun.com/api/article?logo=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&templateTitle=${encodeURIComponent(
       text
     )}&theme=dark`;
   }
