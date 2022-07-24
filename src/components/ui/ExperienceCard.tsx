b6f1643fc4309d19edf8e6050d1b3cca4480e99b
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-07-24 14:40:46 +0700

    feat(ExperienceCard.tsx): :sparkles: create seperate component for experiences

---
+++ src/components/ui/ExperienceCard.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BsBriefcase, BsBuilding } from "react-icons/bs";
+import { GiJourney } from "react-icons/gi";
+
+import clsxm from "@/lib/helpers/clsxm";
+
+type ExperienceCardProps = {
+  experienceName: string;
+  companyName: string;
+  duration: string;
+};
+
+const ExperienceCard = ({
+  experienceName,
+  companyName,
+  duration,
+}: ExperienceCardProps) => {
+  return (
+    <div
+      className={clsxm(
+        "space-y-3 rounded-md",
+        "border border-lightsteel-800 p-3 dark:border-lightsteel-300"
+      )}
+    >
+      <p className="flex items-center gap-2 font-bold">
+        <BsBriefcase size={20} /> {experienceName}
+      </p>
+      <p className="flex items-center gap-2 text-sm">
+        <BsBuilding size={20} /> {companyName}
+      </p>
+      <p className="flex items-center gap-2 font-bold italic">
+        <GiJourney size={20} /> {duration}
+      </p>
+    </div>
+  );
+};
+
+export default ExperienceCard;
