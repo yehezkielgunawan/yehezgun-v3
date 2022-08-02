06d0afcfb808086b9ac8af05c207146b7579e0dd
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-08-02 19:25:13 +0700

    refactor(yehezOGImage): :recycle: change OG Image to simple

--- src/lib/helpers/yehezOgImage.ts
+++ src/lib/helpers/yehezOgImage.ts
@@ -1,16 +1,10 @@
-export default function yehezOgImage(
-  text: string,
-  isArticle: boolean,
-  desc: string
-): string {
+export default function yehezOgImage(text: string, isArticle: boolean): string {
   if (isArticle) {
     return `https://og.yehezgun.com/api/article?logoWidth=124&templateTitle=${encodeURIComponent(
       text
     )}&theme=dark`;
   }
-  return `https://og.yehezgun.com/api/base?description=${encodeURIComponent(
-    desc
-  )}&siteName=yehezgun.com&templateTitle=${encodeURIComponent(
+  return `https://og.yehezgun.com/api/simple?logoWidth=240&siteName=yehezgun.com&templateTitle=${encodeURIComponent(
     text
-  )}&theme=dark&logoWidth=150`;
+  )}`;
 }
