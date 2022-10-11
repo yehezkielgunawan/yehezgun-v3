07f744acd5d47a2e0a51f8b9a7c33f842dd9bacd
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-10-11 19:09:49 +0700

    feat(jest and testing file): :sparkles: add base setup unit test

---
+++ src/__test__/index.test.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+import "@testing-library/jest-dom";
+
+describe("Home page", () => {
+  it("renders home page successfuly", () => {});
+});
