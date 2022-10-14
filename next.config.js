18cdb0b867798ebd3774404fe22c5283631cc408
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-10-14 20:48:58 +0700

    fix(next.config): :bug: use serverruntimeconfig

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  serverRuntimeConfig: {},
-  publicRuntimeConfig: {
+  serverRuntimeConfig: {
     AIRTABLE_APIKEY: process.env.AIRTABLE_APIKEY,
     AIRTABLE_BASE: process.env.AIRTABLE_BASE,
+  },
+  publicRuntimeConfig: {
     UMAMI_WEB_ID: process.env.UMAMI_WEB_ID,
     UMAMI_SRC: process.env.UMAMI_SRC,
   },
--- src/constants/baseConstants.ts
+++ src/constants/baseConstants.ts
@@ -1,8 +1,8 @@
 import getConfig from "next/config";
 
 const { publicRuntimeConfig } = getConfig();
-export const API_KEY = publicRuntimeConfig.AIRTABLE_APIKEY;
-export const AIRTABLE_BASE = publicRuntimeConfig.AIRTABLE_BASE;
+export const API_KEY = process.env.AIRTABLE_APIKEY;
+export const AIRTABLE_BASE = process.env.AIRTABLE_BASE;
 export const CHECK_YOUR_CONNECTION_MESSAGE =
   "Gagal load data, cek koneksi internet anda atau matikan adblock anda!";
 export const UMAMI_WEB_ID = publicRuntimeConfig.UMAMI_WEB_ID;
