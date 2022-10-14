0bbc218b0a67fefca234662730ec0ab7a95fad8f
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-10-14 20:42:37 +0700

    chore(next.config): :wrench: make the API publicruntime

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  serverRuntimeConfig: {
+  serverRuntimeConfig: {},
+  publicRuntimeConfig: {
     AIRTABLE_APIKEY: process.env.AIRTABLE_APIKEY,
     AIRTABLE_BASE: process.env.AIRTABLE_BASE,
-  },
-  publicRuntimeConfig: {
     UMAMI_WEB_ID: process.env.UMAMI_WEB_ID,
     UMAMI_SRC: process.env.UMAMI_SRC,
   },
--- src/constants/baseConstants.ts
+++ src/constants/baseConstants.ts
@@ -1,8 +1,8 @@
 import getConfig from "next/config";
 
-const { publicRuntimeConfig, serverRuntimeConfig } = getConfig();
-export const API_KEY = serverRuntimeConfig.AIRTABLE_APIKEY;
-export const AIRTABLE_BASE = serverRuntimeConfig.AIRTABLE_BASE;
+const { publicRuntimeConfig } = getConfig();
+export const API_KEY = publicRuntimeConfig.AIRTABLE_APIKEY;
+export const AIRTABLE_BASE = publicRuntimeConfig.AIRTABLE_BASE;
 export const CHECK_YOUR_CONNECTION_MESSAGE =
   "Gagal load data, cek koneksi internet anda atau matikan adblock anda!";
 export const UMAMI_WEB_ID = publicRuntimeConfig.UMAMI_WEB_ID;
