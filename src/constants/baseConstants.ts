ec8c82c8888557db28223af4d41f75d5343efc86
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-10-26 20:24:04 +0700

    refactor: :fire: remove unused snippet that still use airtable

--- src/constants/baseConstants.ts
+++ src/constants/baseConstants.ts
@@ -1,5 +1,3 @@
-export const API_KEY = process.env.AIRTABLE_APIKEY;
-export const AIRTABLE_BASE = process.env.AIRTABLE_BASE;
 export const CHECK_YOUR_CONNECTION_MESSAGE =
   "Gagal load data, cek koneksi internet anda atau matikan adblock anda!";
 export const UMAMI_WEB_ID = process.env.UMAMI_WEB_ID;
--- src/lib/services/fetcher.ts
+++ src/lib/services/fetcher.ts
@@ -1,34 +1,4 @@
-import Airtable, { FieldSet, Record, Records } from "airtable";
-
-import { AIRTABLE_BASE, API_KEY } from "constants/baseConstants";
-
 import { getClient } from "./sanity-config";
-
-const base = new Airtable({ apiKey: API_KEY }).base(String(AIRTABLE_BASE));
-
-// maps over the records, calling minifyRecord, giving us required data
-const getMinifiedRecords = (records: Records<FieldSet>) => {
-  return records.map((record: Record<FieldSet>) => minifyRecord(record));
-};
-
-// gets the data we want and puts it into variables
-const minifyRecord = (record: Record<FieldSet>) => {
-  return {
-    id: record.id,
-    fields: record.fields,
-  };
-};
-
-export const getArticlePost = async (slug: string) => {
-  const records = await base("Blog")
-    .select({
-      maxRecords: 1,
-      filterByFormula: `{slug} = "${slug}"`,
-    })
-    .all();
-
-  return getMinifiedRecords(records);
-};
 
 export const getExperienceList = async () => {
   return await getClient(true).fetch(
