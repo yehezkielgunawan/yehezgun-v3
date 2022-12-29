f6840bd7767fd039320f1f6be7c28405fbfc7d1f
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-12-29 21:16:31 +0700

    refactor(baseConstant, sanity-config.ts): :recycle: use env var instead of explicit var

--- src/constants/baseConstants.ts
+++ src/constants/baseConstants.ts
@@ -2,3 +2,5 @@ export const CHECK_YOUR_CONNECTION_MESSAGE =
   "Gagal load data, cek koneksi internet anda atau matikan adblock anda!";
 export const UMAMI_WEB_ID = process.env.UMAMI_WEB_ID;
 export const UMAMI_WEB_SRC = process.env.UMAMI_SRC;
+export const SANITY_DATASET = process.env.SANITY_DATASET;
+export const SANITY_PROJECTID = process.env.SANITY_PROJECTID;
