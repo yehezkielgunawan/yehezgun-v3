24d1a2673b11582171f90c4228fcd04a0fc25141
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-12-29 21:17:04 +0700

    fix(_document.tsx): :bug: use next/script for tracker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   NextScript,
   DocumentContext,
 } from "next/document";
+import Script from "next/script";
 
 import { UMAMI_WEB_ID, UMAMI_WEB_SRC } from "@/constants/baseConstants";
 
@@ -18,13 +19,14 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script
+          <Script
             async
             defer
             data-website-id={UMAMI_WEB_ID}
             src={UMAMI_WEB_SRC}
             data-cache="true"
-          ></script>
+            data-domains="yehezgun.com"
+          ></Script>
           <script src="https://app.embed.im/snow.js" defer></script>
         </Head>
         <body>
