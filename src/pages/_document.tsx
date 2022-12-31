acf18cdb01958ea54e65fa3f9664447bd75e9e02
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-12-31 15:10:36 +0700

    refactor(_document.tsx): :fire: remove the snow and use script tag for tracking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ import Document, {
   NextScript,
   DocumentContext,
 } from "next/document";
-import Script from "next/script";
 
 import { UMAMI_WEB_ID, UMAMI_WEB_SRC } from "@/constants/baseConstants";
 
@@ -17,17 +16,16 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <Script
+          <script
             async
             defer
             data-website-id={UMAMI_WEB_ID}
             src={UMAMI_WEB_SRC}
             data-cache="true"
             data-domains="yehezgun.com"
-          ></Script>
-          <script src="https://app.embed.im/snow.js" defer></script>
+          />
         </Head>
         <body>
           <Main />
