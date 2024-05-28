a5924d1b568121de65be5440c77ba3d6259c692a
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-05-28 09:33:37 +0700

    fix(resume.tsx): :bug: update resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Resume() {
   return (
     <iframe
-      src="https://drive.google.com/file/d/1AdUoO5ElrgrrYLgPPR-kPQWBIyUqQZcg/preview"
+      src="https://drive.google.com/file/d/1yVyJJtsvp0XtHXBR_tZ4cTpdu2gpD0pk/preview"
       className="h-screen w-full"
     ></iframe>
   );
