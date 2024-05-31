9853f7d962876fafcd100471bf1e716db5953708
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-05-31 15:04:29 +0700

    fix(slug.tsx): :bug: minor styling

--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -147,7 +147,7 @@ export default function Post({ postData }: { postData: Article }) {
             />
           </figure>
           <div className="flex flex-wrap items-center justify-between gap-2 md:flex-nowrap md:items-end">
-            <div className="max-w-xl space-y-1">
+            <div className="max-w-2xl space-y-1">
               <h3>{postData.title}</h3>
               <div className="flex items-center gap-3 pb-4">
                 <p className="text-sm italic">
